cf67bdb596eb593faa01f733b964b43841318db5
Matteo Balocco <matteo@matteobalocco.it>
2013-03-29 15:49:57 +0100

    Version 2.0

--- jquery.inputtype.js
+++ jquery.inputtype.js
@@ -5,24 +5,23 @@
  * MIT license
  * Version: 1.0
  *
- * Usage:   $('#input').textfieldType() // type: 'text' is default;
- *          $('#input').textfieldType({type:'tel'});
+ * Usage:   $('#input').inputType() // type: 'text' is default;
+ *          $('#input').inputType({type:'tel'});
  */
-
 ;(function($){
 	var __loop = function(cfg) {
    	$this = $(this)
-   	$this.data( 'textfieldType', cfg );
+   	$this.data( 'inputType', cfg );
     var $newInput = $this.clone();
     var attributes = $this.prop("attributes");
     $.each(attributes, function() {
         $newInput.attr(this.name, this.value);
     });
-    $newInput.attr("type", cfg.type).insertBefore($this).focus();
+    $newInput.attr("type", cfg.type).insertBefore($this);
     $this.unbind('blur').unbind('keyup').remove();
   };
 
-  $.fn.textfieldType = function() {
+  $.fn.inputType = function() {
  		var cfg = false;
  		if ( !arguments.length || $.isPlainObject(arguments[0]) ) {
   		cfg = $.extend({},{type: 'text'},arguments[0]);
